fa8232f2d741aa0484643aa2aeceb2fb35cabddf
dotanGit <frenkeldotan1998@gmail.com>
2025-11-15 16:21:26 +0200

    Comment model
    Created comment model schema

---
+++ models/comment_model.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require("mongoose");
+
+const commentSchema = new Schema({
+    postId: {
+        type: Schema.Types.ObjectId,
+        ref: "Post",
+        required: true
+    },
+    sender: {
+        type: String,
+        required: true,
+        trim: true // remove whitespace from the beginning and end of the string for example "   Dotan   " -> "Dotan"
+    },
+    message: {
+        type: String,
+        required: true,
+        trim: true
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = model("Comment", commentSchema);
